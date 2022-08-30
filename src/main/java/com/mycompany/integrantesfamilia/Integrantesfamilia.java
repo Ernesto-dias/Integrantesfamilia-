4bb9e9d0d93cb8e7e5134d00b2ea629d449bdada
Ernesto-dias <ambrosiomarmol@gmail.com>
2022-08-30 19:00:21 +0200

    PRIMER COMMIT

---
+++ src/main/java/com/mycompany/integrantesfamilia/Integrantesfamilia.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.integrantesfamilia;
+
+/**
+ *
+ * @author ernestodiaz
+ */
+public class Integrantesfamilia {
+
+    public static void main(String[] args) {
+        
+        
+        
+        System.out.println("esta es mi famlia");
+        System.out.println("Mi mama es: Elenena de Diaz");
+        System.out.println("Mi papa es: Alexander Diaz");
+        System.out.println("Mi hermano es: Alexander Diaz");
+        System.out.println("Mi nombre es: Ambrocio Ernesto Diaz Marmol");
+    }
+}
